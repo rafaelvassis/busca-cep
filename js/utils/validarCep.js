3bb3e8c571665d9602c7df3333766ad9b67d6496
Rafael Vasconcelos <955502@sga.pucminas.br>
2026-07-17 23:04:28 -0300

    ajusta validação da função validarCEP para considerar comprimento mínimo e máximo. Retira a normalização.

--- js/utils/validarCep.js
+++ js/utils/validarCep.js
@@ -1,17 +1,9 @@
-import { normalizarCEP } from "./normalizarCep.js";
-
 export function validarCEP(cep) {
-  if (cep === "" || cep.length > 10) return false;
+  if (cep === "" || cep.length > 10 || cep.length < 8) return false;
 
   for (let i = 0; i < cep.length; i++) {
     if (cep[i] !== "." && cep[i] !== "-" && (cep[i] < "0" || cep[i] > "9"))
       return false;
-  }
-
-  const apenasValidos = normalizarCEP(cep);
-
-  if (apenasValidos.length !== 8) {
-    return false;
   }
 
   return true;
